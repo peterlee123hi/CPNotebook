0859be34ba44e49e01caeb6e0b93e5f2b16d2e2e
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-09-02 14:54:58 -0700

    Updated assets

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      title: 'Competitive Programming Notebook'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
