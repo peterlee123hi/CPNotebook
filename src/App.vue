e6f196e71e44eba820a2b202f840560c4133736a
Erik Yang <erikyangs@gmail.com>
2017-09-02 15:26:48 -0700

    background image main  page

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', 'Source Sans Pro', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
